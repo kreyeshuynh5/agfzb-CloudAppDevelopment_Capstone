cfeb0ed466edc6aa0f374625a7e939ba5a96c3d6
Kaianu <kreyeshuynh@gmail.com>
2023-06-22 01:26:25 -0400

    Added Notecard Review

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,8 +1,11 @@
 import requests
-import json
-# import related models here
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features,SentimentOptions
+import time
+import json
 
 
 # Create a `get_request` to make HTTP GET requests
@@ -144,6 +147,16 @@ def get_dealer_reviews_from_cf(url, **kwargs):
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
+def analyze_review_sentiments(text):
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/0263e45d-1fc4-45b3-b540-6fff8cd88869"
+    api_key = "4uQ12ofeaEieiKu6xDLPCUYfX8lW3soX-eSsMNAdSrFX"
+    authenticator = IAMAuthenticator(api_key)
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2022-08-01',authenticator=authenticator)
+    natural_language_understanding.set_service_url(url)
+    response = natural_language_understanding.analyze( text=text+"hello hello hello",features=Features(sentiment=SentimentOptions(targets=[text+"hello hello hello"]))).get_result()
+    label=json.dumps(response, indent=2)
+    label = response['sentiment']['document']['label']
+    
+    return(label)
 
 
